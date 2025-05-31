d0ef3b8fb00866bf5c81cbb85904733e59244d18
tomo-john <keririnkery.kery@gmail.com>
2025-05-31 15:43:45 +0900

    del

--- 2_Page/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const icon = document.getElementById('dog-icon');
-
-  icon.addEventListener('click', function () {
-    icon.classList.toggle('move'); // クラスの付け外しで動きを制御
-  });
-});
-
